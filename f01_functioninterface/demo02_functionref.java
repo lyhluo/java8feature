285ceb74e060c3a1ba74b7c3a77edcc862c32b66
罗迎豪 <luoyinghao@outlook.com>
2018-04-03 09:57:32 +0800

    lambda的引用和域

--- f01_functioninterface/demo02_functionref.java
+++ f01_functioninterface/demo02_functionref.java
@@ -24,32 +24,121 @@ public class demo02_functionref {
 
         Convertor<String, Integer> convertor = (param -> str2int(param));
 
-        // 应用在静态引用
+        // 1 应用在静态引用
         Convertor<String, Integer> staticRef = demo02_functionref::str2int;
 
         Integer convert = staticRef.convert("200");
 
         System.out.println(convert);
 
-        // 应用在实例方法引用
+        // 2 应用在实例方法引用
 //        Helper helper = new Helper();
 //        Convertor<String, Integer> shiliRef = (param) -> helper::str2int(param);// 错误写法
 
         Helper helper = new Helper();
         Convertor<String, Integer> converter = helper::str2int;
         converter.convert("120");
+
+        // 3 在构造方法中的引用
+        applyinConstructor();
+
+
     }
 
-    // 配合静态引用
+    // 1 配合静态方法引用
     static Integer str2int(String param) {
         return Integer.valueOf(param);
     }
 
 
-    static class Helper {
+    //2 配合实例方法引用
+    static class Helper {// 由于在main方法中引用, 因此要加static. 因为main是static的
         public Integer str2int(String param) {
             return Integer.valueOf(param);
         }
     }
+
+    // 3 在构造方法中的引用
+    private static void applyinConstructor() {
+        // 传统方式:
+
+        AnimalFactory birdFactory = new AnimalFactory() {
+            @Override
+            public Bird create(String name, Integer age) {
+                return new Bird(name, age);
+            }
+        };
+        Bird bird = (Bird) birdFactory.create("", 0);
+
+        AnimalFactory dogFactory = new AnimalFactory() {
+            @Override
+            public Dog create(String name, Integer age) {
+                return new Dog(name, age);
+            }
+        };
+
+        Dog dog = (Dog) dogFactory.create("", 1);
+
+
+        // lambda方法:
+
+        AnimalFactory birdFactoryLambda = Bird::new;
+
+        birdFactoryLambda.create("", 9);
+
+        AnimalFactory dogFactoryLambda = Dog::new;
+
+        dogFactoryLambda.create("", 1);
+    }
+
+
+
+}
+
+// 3 配合构造方法引用
+class Animal {
+    private String name;
+    private Integer age;
+
+    public Animal(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
+
+class Bird extends Animal{
+
+    public Bird(String name, Integer age) {
+        super(name, age);
+    }
+}
+
+class Dog extends Animal{
+
+    public Dog(String name, Integer age) {
+        super(name, age);
+    }
+}
+
+@FunctionalInterface
+interface AnimalFactory{
+
+    Animal create(String name, Integer age);
 
 }
