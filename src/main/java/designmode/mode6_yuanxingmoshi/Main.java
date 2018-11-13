9d9ed9cfcf8f24b1205cf05cd988663794e64c1b
crab <xylxie@126.com>
2018-11-13 17:26:58 +0800

    原型模式(深拷贝，浅拷贝）

--- src/main/java/designmode/mode6_yuanxingmoshi/Main.java
+++ src/main/java/designmode/mode6_yuanxingmoshi/Main.java
@@ -1,5 +1,10 @@
 package designmode.mode6_yuanxingmoshi;
 
+/**
+ * 原型模式
+ * 对象克隆
+ * 深复制，浅复制
+ */
 public class Main {
     public static void main(String[] args) {
 
---
+++ src/main/java/designmode/mode7_mobanfangfamoshi/Main.java
@@ -0,0 +1,8 @@
+package designmode.mode7_mobanfangfamoshi;
+
+/**
+ * 模版方法模式
+ *
+ */
+public class Main {
+}
