61d19c0b546159fcdee72e9aa57fefdfb603aaef
crab <xylxie@126.com>
2018-11-19 14:46:59 +0800

    理解原型模式和模板方法模式

--- src/main/java/designmode/mode6_yuanxingmoshi/Main.java
+++ src/main/java/designmode/mode6_yuanxingmoshi/Main.java
@@ -3,9 +3,10 @@ package designmode.mode6_yuanxingmoshi;
 /**
  * 原型模式
  * 对象克隆
- * 深复制，浅复制
+ * 深复制，引用类型成员变量值传递（利用Cloneable或者利用序列化和反序列化进行深拷贝）
+ * 浅复制，引用类型成员变量引用传递（利用Cloneable）
  */
-public class Main {
+public class Main implements Cloneable{
     public static void main(String[] args) {
 
     }
