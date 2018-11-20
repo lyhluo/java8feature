af1ac640e300ed1edd5c572bf5c834f7282af66c
crab <xylxie@126.com>
2018-11-20 17:34:29 +0800

    理解外观模式

--- src/main/java/designmode/mode8_waiguanmoshi/Main.java
+++ src/main/java/designmode/mode8_waiguanmoshi/Main.java
@@ -2,7 +2,8 @@ package designmode.mode8_waiguanmoshi;
 
 /**
  * 外观模式
- *
+ * 接口与实现分开，使得调用更加容易和清晰，也便于后期维护
+ * 典型例子：三层架构每层之间也是接口调用，例如将service层拆分为接口和实现
  */
 public class Main {
 
---
+++ src/main/java/designmode/mode9_jianzaozhemoshi/Main.java
@@ -0,0 +1,8 @@
+package designmode.mode9_jianzaozhemoshi;
+
+/**
+ * 建造者模式
+ *
+ */
+public class Main {
+}
